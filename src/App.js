d6a2267fc27e957ac0f1dafa9d3fdf87e09d2f62
Chris <chris.ware.dev@gmail.com>
2021-08-22 15:34:10 -0700

    Question toggle functionality/completion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from './data'
-import SingleQuetion from './Question'
+import SingleQuestion from './Question'
 
 
 function App() {
+  const [questions, setQuestions] = useState(data);
+  
   return (
-    <div>
-      Home
-    </div>
+    <main>
+      <div className="container">
+        <h3>Questions and Answers about Login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
